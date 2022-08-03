1c2e30ecfe2060f16c83ea774c381c73077e4269
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-08-03 16:29:30 +0900

    merge전 수정
    merge전 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logout } from "../redux/modules/user";
 
 const Header = () => {
