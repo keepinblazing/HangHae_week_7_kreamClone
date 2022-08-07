e3fbea88e3a1dbada4ec331005f8d66fb0e31ae5
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-08-07 17:39:14 +0900

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://ec2-54-180-121-207.ap-northeast-2.compute.amazonaws.com:8080",
+  baseURL: "",
 });
 
 export default instance;
